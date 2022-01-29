2fc32a5db363b3fbc3b936e5e8c1236f1d0b4229
Slin Castro <scastro@thoughtworks.com>
2022-01-28 19:38:13 -0500

    [#2] rename variables

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 from src.configuration.configuration import Configuration
 from src.consoleDrawings.titles import display_title_bar
+from src.filters.CommitsTimeLine import CommitsTimeline
 from src.managers.json_manager import generate_json
 from src.managers.projectManager import ProjectManager
 
@@ -21,10 +22,8 @@ commits_file_path = project_manager.json_path
 generate_json(repo_configuration, commits_file_path)
 
 commitsDf = pd.read_json(commits_file_path)
-commitsDf['date'] = pd.to_datetime(commitsDf['date'], utc=True)
-commitsDf = commitsDf.set_index(pd.DatetimeIndex(commitsDf['date']))
 
-commits_by_week = commitsDf.resample('W').size().to_frame('NumberOfCommits').reset_index()
+commits_peer_week = CommitsTimeline().get_commits_per_week(commitsDf)
 
 print(F"Project Name : {project_requested_name}")
 
@@ -32,7 +31,7 @@ commits_per_dev = commitsDf.groupby('name').size().to_frame('NumberOfCommits').r
 
 print(commits_per_dev)
 
-plt.bar(commits_by_week["date"], height=commits_by_week["NumberOfCommits"])
+plt.bar(commits_peer_week["date"], height=commits_peer_week["NumberOfCommits"])
 
 plt.show()
 
